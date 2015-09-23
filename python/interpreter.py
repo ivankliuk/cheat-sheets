2b38de4ea703249ed67f393b63aa42b1ae1b2b46
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-09-24 02:35:14 +0300

    Add Python interpreter options

---
+++ python/interpreter.py
@@ -0,0 +1,16 @@
+Python interpreter
+==================
+
+Keep the interpreter session open after the script is done:
+
+.. code :: bash
+
+  python -i prog.py
+
+Run script within `pdb <https://github.com/ivankliuk/cheat-sheets/blob/master/python/pdb.rst>`_:
+
+.. code :: bash
+
+  python -m pdb prog.py
+
+
